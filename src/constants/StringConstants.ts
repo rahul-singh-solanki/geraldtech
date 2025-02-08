fec05b8a9b1253afa185bf66d85670aa8fc471b4
Rahul Singh Solanki <rs.singh1812@gmail.com>
2025-02-09 01:12:56 +0530

    Add StringContants & Colors

---
+++ src/constants/StringConstants.ts
@@ -0,0 +1,19 @@
+const StringConstants = {
+  SCREENS: {
+    HOME: 'Home',
+    SCREEN_1: 'Screen1',
+    SCREEN_2: 'Screen2',
+    START: 'Start',
+    YOUR_CART: 'Your Cart',
+    FAVOURITES: 'Favourites',
+    YOUR_ORDERS: 'Your Orders',
+    CONTACT: 'Contact',
+  },
+  BUTTONS: {
+    GO_TO_SCREEN_1: 'Go to Screen 1',
+    GO_TO_SCREEN_2: 'Go to Screen 2',
+  },
+  SIGN_OUT_DOESNT_EXISTS: "Sign out functionality doesn't exists",
+}
+
+export default StringConstants
---
+++ src/utils/Colors.ts
@@ -0,0 +1,10 @@
+const Colors = {
+  CraterBrown: '#432523',
+  Froly: '#f46c71',
+  Ebony: '#12122d',
+  White: '#ffffff',
+  Black: '#000000',
+  Transparent: 'transparent',
+}
+
+export default Colors
